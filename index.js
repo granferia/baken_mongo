26495dd3681e8fa9b354ea1bd6dc366eee637d2f
juan <66888856+granferia@users.noreply.github.com>
2022-12-05 23:42:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express =require('express')
+const mongoose =require('mongoose')
+const bodyParser=require('body-parser')
+const cors = require('cors')
+const routes = require('./routes')
+
+
+
+const app = express()
+mongoose.Promise=global.Promise
+mongoose.connect(
+    'mongodb://131.221.41.58:30000/deportes',
+    {useNewUrlParser:true}
+    )
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
+app.use('/',routes())
+
+//app.get('/',(req,res)=>{
+ //   res.send('mi mongo 2024')
+//})
+app.listen(5000,()=>{
+    console.log('server listen 5000')
+})
